031d5f9db3cfc532f7f86c8d385bdb803fc2f9fd
Jose Alfonso <Jose Alfonso@DESKTOP-LAVI9KC>
2021-12-22 17:45:12 -0600

    Agregar archivos de proyecto.

---
+++ Contador/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                float num = 0.0f;
+                Console.WriteLine("Contaremos 100 numeros a partir del digito que ingrese \n");
+                Console.WriteLine("Escriba un numero");
+                num = float.Parse(Console.ReadLine());
+
+                for (float n = 1; n<= 100; n++)
+                {
+                    num = 1 + num;
+                    Console.WriteLine(num);
+                }
+                Console.ReadKey();
+
+            }
+            catch(Exception)
+                {
+                Console.WriteLine("Inserte un numero no un caracter");
+                }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Contadorwhile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contadorwhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                float num = 0.0f;
+                float num2 = 0.0f;
+                Console.WriteLine("Contaremos 100 numeros a partir del digito que ingrese \n");
+                Console.WriteLine("Escriba un numero");
+                num = float.Parse(Console.ReadLine());
+
+                do
+                {
+                    num2++;
+                    Console.WriteLine(num + num2);
+                } 
+                while (num2 < 100);
+                Console.ReadKey();
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Digite un numero no un caracter");
+            }
+            Console.ReadLine(); 
+                
+        }
+    }
+}
